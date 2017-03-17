06664dd3872df9a3a908d7b7ef57fa98a8076f3a
Irfan Yunus <iyunusp@gmail.com>
2017-03-17 14:52:27 +0700

    Update login.php
    change table database

--- login.php
+++ login.php
@@ -1,11 +1,11 @@
 <?php
-    $con=mysqli_connect("mysql4.gear.host","aea","Xd4Y-2qA!7vr","aea");
+    $con=mysqli_connect("mysql4.gear.host","aea","Xd4Y-2qA!7vr","userData");
     if(mysqli_connect_errno($con)){
         echo "failed to connect" . mysqli_connect_error();
 	}
     $username=$_GET['username'];
     $password=$_GET['password'];
-    $result=mysqli_query($con,"SELECT Name from AeA where Name like '$username%' and Game like '$password%'");
+    $result=mysqli_query($con,"SELECT userName,userRealname,userMail,userPhone,userAddress from AeA where userName like '$username' and userPassword like '$password'");
     $row=mysqli_fetch_array($result);
     $data=$row[0];
     if($data){
