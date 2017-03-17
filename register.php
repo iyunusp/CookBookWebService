dd4044ae2cf2fe71410e071f80db2ce7a7456421
Irfan Yunus <iyunusp@gmail.com>
2017-03-17 20:23:19 +0700

    Update register.php

--- register.php
+++ register.php
@@ -3,10 +3,10 @@
     if(mysqli_connect_errno($con)){
         echo "failed to connect" . mysqli_connect_error();
 	}
-    $username=$_GET['username'];
-	$password=$_GET['password'];
-	$realname=$_GET['realname'];
-	$mail=$_GET['mail'];
+    $username=$_POST['username'];
+	$password=$_POST['password'];
+	$realname=$_POST['realname'];
+	$mail=$_POST['mail'];
     $result=mysqli_query($con,"INSERT INTO usercook(userName,userPassword,userRealname,userMail,userPhone,userAddress) VALUES '$username','$password','$realname','$mail','unknown','unknown'");
     mysqli_close($con);
 ?>
