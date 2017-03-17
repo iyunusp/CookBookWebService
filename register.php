babe92b4cf43136df4c205b122776b80d18703c6
Irfan Yunus <iyunusp@gmail.com>
2017-03-17 20:08:43 +0700

    Add files via upload
    register service

---
+++ register.php
@@ -0,0 +1,12 @@
+<?php
+    $con=mysqli_connect("mysql4.gear.host","usercook","Il5A1Y73~!KQ","usercook");
+    if(mysqli_connect_errno($con)){
+        echo "failed to connect" . mysqli_connect_error();
+	}
+    $username=$_GET['username'];
+	$password=$_GET['password'];
+	$realname=$_GET['realname'];
+	$mail=$_GET['mail'];
+    $result=mysqli_query($con,"INSERT INTO usercook(userName,userPassword,userRealname,userMail,userPhone,userAddress) VALUE '$username','$password','$realname','$mail','unknown','unknown'");
+    mysqli_close($con);
+?>
