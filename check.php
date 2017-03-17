3edfbb3c56248de7a62cdc76227734aa3cbf3986
Irfan Yunus <iyunusp@gmail.com>
2017-03-17 18:27:15 +0700

    Update check.php

--- check.php
+++ check.php
@@ -4,7 +4,7 @@
         echo "failed to connect" . mysqli_connect_error();
 	}
     $username=$_GET['username'];
-    $result=mysqli_query($con,"SELECT userName from usercook where userName like '$username');
+    $result=mysqli_query($con,"SELECT userName from usercook where userName like '$username'");
     $row=mysqli_fetch_array($result);
     $data=$row[0];
     if($data){
