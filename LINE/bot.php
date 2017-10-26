4f8540817617c535988c3e64bd6216f08bde70d5
Irfan Yunus <iyunusp@gmail.com>
2017-10-27 03:24:06 +0700

    fix n attempt n

--- LINE/bot.php
+++ LINE/bot.php
@@ -32,7 +32,10 @@ foreach ($client->parseEvents() as $event) {
 						$result=mysqli_query($con,"SELECT Name, Price from games where Name like '%$clientText%'");
 						$clientText='';
 						for($i=1;$row=mysqli_fetch_array($result);$i++){
-							$clientText .= $i . '\t' . $row[0] . '\t' . $row[1] . "\r\n";
+							if($i==1){
+								$clientText .= sprintf("%-2s %-25s %-7s \r\n",'No' , 'Name' , 'Price');
+							}
+							$clientText .= sprintf("%2d %-25s %-7d\r\n",$i , $row[0] , $row[1]);
 						}
 						if($clientText==''){
 							$client->replyMessage(array(
